0ffee15b812852c5974a8c2c60d62b116f7e4872
Malcolm <malcolmclaing@gmail.com>
2019-08-04 23:31:28 +0200

    chore: remove unused file

--- packages/client/src/components/primary-button/primary-button.js
+++
@@ -1,8 +0,0 @@
-import styled from "@emotion/styled";
-
-const PrimaryButton = styled.button`
-  border-radius: ${props => props.theme.borderRadius};
-  background-color: "blue";
-`;
-
-export default PrimaryButton;
